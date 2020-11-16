3d9e565ec183124695324801622bf36e6aeb6b9d
Andrea Massetti <Massetti@Sierentz-gm.com>
2020-11-16 13:03:44 +0100

    added random seed to keep the values in the csv the same at every run [NOT TESTED]

--- write documentation/manipulate_csv.py
+++ write documentation/manipulate_csv.py
@@ -13,8 +13,8 @@ LOC = Path(r"C:\Users\massetti\OneDrive - Sierentz & CIE\Documents\Python Script
 
 def create_dummy_csv(random_seed=3):
     the_csv = pd.DataFrame(columns=["test1", "test2"])
-    the_csv["test1"] = np.random.rand(10)#, seed=random_seed)
-    the_csv["test2"] = np.random.rand(10)
+    the_csv["test1"] = np.random.rand(10, seed=random_seed)
+    the_csv["test2"] = np.random.rand(10, seed=random_seed)
     the_csv.to_csv(str(LOC / "write documentation" / "the_csv.csv"), index=False)
     print("saved the csv at {}".format(LOC))#TODO! should I have LOC as an argument?
     return True
