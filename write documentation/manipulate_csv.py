ee59abc7aafb68e026916588f2978a5cecfa5541
Andrea Massetti <Massetti@Sierentz-gm.com>
2020-11-16 13:06:24 +0100

    initialized the documentation string

--- write documentation/manipulate_csv.py
+++ write documentation/manipulate_csv.py
@@ -12,8 +12,12 @@ LOC = Path(r"C:\Users\massetti\OneDrive - Sierentz & CIE\Documents\Python Script
 
 
 def create_dummy_csv(random_seed=3):
+    """
+    @Roman could you please write the right documentation for this function?
+
+    """
     the_csv = pd.DataFrame(columns=["test1", "test2"])
-    the_csv["test1"] = np.random.rand(10, seed=random_seed)
+    the_csv["test1"] = np.random.rand(10, seed=random_seed)# not sure if the seed is a correct input to np.random.rand, could you check it for me please?
     the_csv["test2"] = np.random.rand(10, seed=random_seed)
     the_csv.to_csv(str(LOC / "write documentation" / "the_csv.csv"), index=False)
     print("saved the csv at {}".format(LOC))#TODO! should I have LOC as an argument?
