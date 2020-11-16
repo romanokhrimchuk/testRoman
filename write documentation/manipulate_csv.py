58a0eda7bcc19626c73e19a9c1fc20046c940364
Andrea Massetti <Massetti@Sierentz-gm.com>
2020-11-16 13:11:13 +0100

    created function to read the csv

--- write documentation/manipulate_csv.py
+++ write documentation/manipulate_csv.py
@@ -22,3 +22,9 @@ def create_dummy_csv(random_seed=3):
     the_csv.to_csv(str(LOC / "write documentation" / "the_csv.csv"), index=False)
     print("saved the csv at {}".format(LOC))#TODO! should I have LOC as an argument?
     return True
+
+def read_the_csv(name_of_the_csv):
+    the_csv = pd.read_csv(name_of_the_csv)
+    return the_csv
+
+#    the_csv.plot()
