8ccf4ce457af95ac8e1104f75198f03ed180f4e1
Roman Okhrimchuk <romzes00005@gmail.com>
2020-11-20 05:15:39 +0200

    add comment to the create_dummy_csv function

--- write documentation/manipulate_csv.py
+++ write documentation/manipulate_csv.py
@@ -12,9 +12,28 @@ import os
 def create_dummy_csv(outname_csv='the_csv', output_path=None, 
                      random_seed=None, columns=["test1", "test2"], 
                      num_column_elements=10):
-    """
-    @Roman could you please write the right documentation for this function?
-
+    """  
+    The function creates .csv with randomly generated dummy data
+    
+    Parameters
+    ----------
+    outname_csv : str, default 'the_csv'
+        The name with which the file will be save
+    output_path : str, default None
+        The path where the .csv file will be save. Current working 
+        directory will be take as the path, if this parameter is not fill
+    random_seed : int or 1-d array_like, default None
+        Value of this parameter to use for initialization the random number 
+        generator. The random number generator will start with it value, to
+        be able to generate a random number. By default the random number 
+        generator uses the current system time.
+        Note: The parameter random_seed must be convertible to 32 bit unsigned 
+        integers
+    columns : list, default ["test1", "test2"]
+        The name of columns which will be create
+    num_column_elements : int, default 10
+        Generates a given number of random values using the uniform 
+        distribution over [0, 1) for every column
     """
     np.random.seed(random_seed)
     the_csv = pd.DataFrame(columns = columns, data = np.random.rand(num_column_elements,len(columns)))
